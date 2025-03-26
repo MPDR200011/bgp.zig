cb896269aa492ef1646f29d0383a7a1516dbece8
Moisés Rocha <moisesrocha99@gmail.com>
2025-03-26 18:53:19 +0000

    Add logging settings

--- src/main.zig
+++ src/main.zig
@@ -17,6 +17,35 @@ const bgpEncoding = @import("messaging/encoding/encoder.zig");
 const PeerSessionAddresses = session.PeerSessionAddresses;
 const Peer = session.Peer;
 
+pub const std_options: std.Options = .{
+    // Set the log level to info
+    .log_level = .info,
+
+    // Define logFn to override the std implementation
+    .logFn = myLogFn,
+};
+
+pub fn myLogFn(
+    comptime level: std.log.Level,
+    comptime scope: @Type(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    // Ignore all non-error logging from sources other than
+    // .my_project, .nice_library and the default
+    const scope_prefix = "(" ++ @tagName(scope) ++ "): ";
+
+    comptime var upperLevel = [_]u8{ 0 };
+    comptime _ = std.ascii.upperString(&upperLevel, level.asText()[0..1]);
+    const prefix = comptime upperLevel ++ " " ++ scope_prefix;
+
+    // Print the message to stderr, silently ignoring any errors
+    std.debug.lockStdErr();
+    defer std.debug.unlockStdErr();
+    const stderr = std.io.getStdErr().writer();
+    nosuspend stderr.print(prefix ++ format ++ "\n", args) catch return;
+}
+
 pub const PeerMapCtx = struct {
     const Self = @This();
 
