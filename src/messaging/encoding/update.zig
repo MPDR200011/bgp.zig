74521d11575b88e3311d1d5ed1dfc6dc48376325
Moisés Rocha <moisesrocha99@gmail.com>
2026-03-14 18:42:25 +0000

    Add complete test for encoding message

--- src/messaging/encoding/update.zig
+++ src/messaging/encoding/update.zig
@@ -236,67 +236,19 @@ test "writeRoutes()" {
     try testing.expectEqualSlices(u8, &expectedBuffer, writtenBuffer);
 }
 
-test "writeUpdateBody() - External Peer" {
-    const asPath = asPathInit: {
-        const referencePath: AsPath = .{
-            .allocator = testing.allocator,
-            .segments = &[_]ribModel.ASPathSegment{
-                .{.allocator = testing.allocator, .segType = .AS_Set, .contents = &[_]u16{69}}
-            },
-        };
-        break :asPathInit try referencePath.clone(testing.allocator);
-    };
-
-    var list = std.ArrayListUnmanaged(PathAttribute){};
-    try list.append(testing.allocator, .{ .Origin = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = .EGP } });
-    try list.append(testing.allocator, .{ .AsPath = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = asPath } });
-    try list.append(testing.allocator, .{ .Nexthop = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = ip.IpV4Address.init(0, 0, 0, 0) } });
+test "writeUpdateBody() - Minimal (Withdrawn only)" {
+    const list = std.ArrayListUnmanaged(PathAttribute){};
     const attrs = AttributeList{ .alloc = testing.allocator, .list = list };
 
     var msg = try messageModel.UpdateMessage.init(
         testing.allocator, 
-        &[_]ribModel.Route{ 
-            ribModel.Route{
-                .prefixLength = 16,
-                .prefixData = [4]u8{ 0xff, 0xff, 0, 0 },
-            }, 
-            ribModel.Route{
-                .prefixLength = 12,
-                .prefixData = [4]u8{ 0, 0xff, 0, 0 },
-            } 
-        }, 
         &[_]ribModel.Route{
-            ribModel.Route{
-                .prefixLength = 32,
-                .prefixData = [4]u8{ 127, 0, 42, 69 },
-            },
-            ribModel.Route{
-                .prefixLength = 31,
-                .prefixData = [4]u8{ 127, 0, 42, 69 },
-            },
+            .{ .prefixLength = 24, .prefixData = [4]u8{ 10, 0, 0, 0 } },
         }, 
+        &[_]ribModel.Route{}, 
         attrs,
     );
     defer msg.deinit();
-
-    // zig fmt: off
-    const expectedBuffer = [_]u8{ 
-        // Withdrawn
-        0, 6,
-        16, 0xff, 0xff, 
-        12, 0, 0xff, 
-        // Attrs
-        0, 4 + 7 + 7, 
-        // -- Origin (Flags:0x40, Type:1, Len:1, Val:1)
-        0x40, 1, 1, 1,
-        // -- As Path (Flags:0x40, Type:2, Len:4, Val: [AS_SET, 1 ASN, 69])
-        0x40, 2, 4, 1, 1, 0, 69,
-        // -- Next hop (Flags:0x40, Type:3, Len:4, Val: 0.0.0.0)
-        0x40, 3, 4, 0, 0, 0, 0, 
-        // Adv
-        32, 127, 0, 42, 69, 
-        31, 127, 0, 42, 69 
-    };
 
     var writer = std.Io.Writer.Allocating.init(testing.allocator);
     defer writer.deinit();
@@ -305,75 +257,68 @@ test "writeUpdateBody() - External Peer" {
 
     const writtenBuffer = try writer.toOwnedSlice();
     defer testing.allocator.free(writtenBuffer);
+
+    // zig fmt: off
+    const expectedBuffer = [_]u8{ 
+        0, 4,       // Withdrawn len: 4
+        24, 10, 0, 0, 
+        0, 0        // Attrs len: 0
+    };
     try testing.expectEqualSlices(u8, &expectedBuffer, writtenBuffer);
 }
 
-test "writeUpdateBody() - Internal Peer" {
+
+test "writeUpdateBody() - All Attributes" {
     const asPath = asPathInit: {
         const referencePath: AsPath = .{
             .allocator = testing.allocator,
             .segments = &[_]ribModel.ASPathSegment{
-                .{.allocator = testing.allocator, .segType = .AS_Set, .contents = &[_]u16{69}}
+                .{.allocator = testing.allocator, .segType = .AS_Sequence, .contents = &[_]u16{65001}}
             },
         };
         break :asPathInit try referencePath.clone(testing.allocator);
     };
 
+    const unknownExtData = try testing.allocator.alloc(u8, 300);
+    @memset(unknownExtData, 0);
+    defer testing.allocator.free(unknownExtData);
+
     var list = std.ArrayListUnmanaged(PathAttribute){};
-    try list.append(testing.allocator, .{ .Origin = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = .EGP } });
+    try list.append(testing.allocator, .{ .Origin = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = .IGP } });
     try list.append(testing.allocator, .{ .AsPath = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = asPath } });
-    try list.append(testing.allocator, .{ .Nexthop = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = ip.IpV4Address.init(0, 0, 0, 0) } });
-    try list.append(testing.allocator, .{ .LocalPref = .init(100) });
+    try list.append(testing.allocator, .{ .Nexthop = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = ip.IpV4Address.init(1, 2, 3, 4) } });
+    try list.append(testing.allocator, .{ .MultiExitDiscriminator = .{ .flags = ribModel.ATTR_OPTIONAL_FLAG, .value = 1234 } });
+    try list.append(testing.allocator, .{ .LocalPref = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = 100 } });
+    try list.append(testing.allocator, .{ .AtomicAggregate = .{ .flags = ribModel.ATTR_TRANSITIVE_FLAG, .value = true } });
+    try list.append(testing.allocator, .{ .Aggregator = .{ 
+        .flags = ribModel.ATTR_OPTIONAL_FLAG | ribModel.ATTR_TRANSITIVE_FLAG, 
+        .value = .{ .as = 65001, .address = ip.IpV4Address.init(1, 2, 3, 4) } 
+    } });
+    try list.append(testing.allocator, .{ .Unknown = .{ 
+        .flags = ribModel.ATTR_OPTIONAL_FLAG, 
+        .value = .{ .allocator = testing.allocator, .typeCode = 100, .value = try testing.allocator.dupe(u8, &[_]u8{ 1, 2, 3 }) } 
+    } });
+    try list.append(testing.allocator, .{ .Unknown = .{ 
+        .flags = ribModel.ATTR_OPTIONAL_FLAG, 
+        .value = .{ .allocator = testing.allocator, .typeCode = 101, .value = try testing.allocator.dupe(u8, unknownExtData) } 
+    } });
+
     const attrs = AttributeList{ .alloc = testing.allocator, .list = list };
 
     var msg = try messageModel.UpdateMessage.init(
         testing.allocator, 
-        &[_]ribModel.Route{ 
-            ribModel.Route{
-                .prefixLength = 16,
-                .prefixData = [4]u8{ 0xff, 0xff, 0, 0 },
-            }, 
-            ribModel.Route{
-                .prefixLength = 12,
-                .prefixData = [4]u8{ 0, 0xff, 0, 0 },
-            } 
+        &[_]ribModel.Route{
+            .{ .prefixLength = 24, .prefixData = [4]u8{ 192, 168, 1, 0 } },
         }, 
         &[_]ribModel.Route{
             ribModel.Route{
                 .prefixLength = 32,
-                .prefixData = [4]u8{ 127, 0, 42, 69 },
-            },
-            ribModel.Route{
-                .prefixLength = 31,
-                .prefixData = [4]u8{ 127, 0, 42, 69 },
+                .prefixData = [4]u8{ 127, 0, 0, 1 },
             },
         }, 
         attrs,
     );
-    // Let's set the flags on localPref to 0x40 (Transitive) just in case, though it's typically 0x40 for well-known
-    // Actually the default flags is 0, let's keep it 0 as the other test doesn't set it 
     defer msg.deinit();
-
-    // zig fmt: off
-    const expectedBuffer = [_]u8{ 
-        // Withdrawn
-        0, 6,
-        16, 0xff, 0xff, 
-        12, 0, 0xff, 
-        // Attrs
-        0, 4 + 7 + 7 + 7, 
-        // -- Origin (Flags:0x40, Type:1, Len:1, Val:1)
-        0x40, 1, 1, 1,
-        // -- As Path (Flags:0x40, Type:2, Len:4, Val: [AS_SET, 1 ASN, 69])
-        0x40, 2, 4, 1, 1, 0, 69,
-        // -- Next hop (Flags:0x40, Type:3, Len:4, Val: 0.0.0.0)
-        0x40, 3, 4, 0, 0, 0, 0, 
-        // -- Local Pref (Flags:0x00, Type:5, Len:4, Val: 100)
-        0x00, 5, 4, 0, 0, 0, 100,
-        // Adv
-        32, 127, 0, 42, 69, 
-        31, 127, 0, 42, 69 
-    };
 
     var writer = std.Io.Writer.Allocating.init(testing.allocator);
     defer writer.deinit();
@@ -382,5 +327,69 @@ test "writeUpdateBody() - Internal Peer" {
 
     const writtenBuffer = try writer.toOwnedSlice();
     defer testing.allocator.free(writtenBuffer);
-    try testing.expectEqualSlices(u8, &expectedBuffer, writtenBuffer);
+
+    // Header: 
+    // Withdrawn len: 0 (2 bytes)
+    // Attrs len: ... (2 bytes)
+    // Attrs:
+    // - Origin: 0x40, 1, 1, 0 (4 bytes)
+    // - As Path: 0x40, 2, 4, 2, 1, 0xFD, 0xE9 (7 bytes)
+    // - Nexthop: 0x40, 3, 4, 1, 2, 3, 4 (7 bytes)
+    // - MED: 0x80, 4, 4, 0, 0, 4, 210 (7 bytes)
+    // - LocalPref: 0x40, 5, 4, 0, 0, 0, 100 (7 bytes)
+    // - AtomicAggregate: 0x40, 6, 0 (3 bytes)
+    // - Aggregator: 0xC0, 7, 6, 0xFD, 0xE9, 1, 2, 3, 4 (9 bytes)
+    // - Unknown (100): 0x80, 100, 3, 1, 2, 3 (6 bytes)
+    // - Unknown (101): 0x90, 101, 1, 44, [0]*300 (4 + 300 = 304 bytes)
+    // Adv: 32, 127, 0, 0, 1 (5 bytes)
+
+    const expected_attrs_len = 4 + 7 + 7 + 7 + 7 + 3 + 9 + 6 + 304; // 354
+    try testing.expectEqual(4, std.mem.readInt(u16, writtenBuffer[0..2], .big)); // Withdrawn len
+    try testing.expectEqualSlices(u8, &[_]u8{ 24, 192, 168, 1 }, writtenBuffer[2..6]);
+    try testing.expectEqual(expected_attrs_len, std.mem.readInt(u16, writtenBuffer[6..8], .big)); // Attrs len
+    
+    // Check specific attributes
+    var offset: usize = 8;
+    
+    // Origin
+    try testing.expectEqualSlices(u8, &[_]u8{ 0x40, 1, 1, 0 }, writtenBuffer[offset .. offset + 4]);
+    offset += 4;
+
+    // AsPath
+    try testing.expectEqualSlices(u8, &[_]u8{ 0x40, 2, 4, 2, 1, 0xFD, 0xE9 }, writtenBuffer[offset .. offset + 7]);
+    offset += 7;
+
+    // Nexthop
+    try testing.expectEqualSlices(u8, &[_]u8{ 0x40, 3, 4, 1, 2, 3, 4 }, writtenBuffer[offset .. offset + 7]);
+    offset += 7;
+
+    // MED
+    try testing.expectEqualSlices(u8, &[_]u8{ 0x80, 4, 4, 0, 0, 4, 210 }, writtenBuffer[offset .. offset + 7]);
+    offset += 7;
+
+    // LocalPref
+    try testing.expectEqualSlices(u8, &[_]u8{ 0x40, 5, 4, 0, 0, 0, 100 }, writtenBuffer[offset .. offset + 7]);
+    offset += 7;
+
+    // AtomicAggregate
+    try testing.expectEqualSlices(u8, &[_]u8{ 0x40, 6, 0 }, writtenBuffer[offset .. offset + 3]);
+    offset += 3;
+
+    // Aggregator
+    try testing.expectEqualSlices(u8, &[_]u8{ 0xC0, 7, 6, 0xFD, 0xE9, 1, 2, 3, 4 }, writtenBuffer[offset .. offset + 9]);
+    offset += 9;
+
+    // Unknown (100)
+    try testing.expectEqualSlices(u8, &[_]u8{ 0x80, 100, 3, 1, 2, 3 }, writtenBuffer[offset .. offset + 6]);
+    offset += 6;
+
+    // Unknown (101) - Extended
+    try testing.expectEqual(0x90, writtenBuffer[offset]);
+    try testing.expectEqual(101, writtenBuffer[offset + 1]);
+    try testing.expectEqual(@as(u16, 300), std.mem.readInt(u16, &[_]u8{writtenBuffer[offset + 2], writtenBuffer[offset + 3]}, .big));
+    try testing.expectEqualSlices(u8, unknownExtData, writtenBuffer[offset + 4 .. offset + 304]);
+    offset += 304;
+
+    // Adv
+    try testing.expectEqualSlices(u8, &[_]u8{ 32, 127, 0, 0, 1 }, writtenBuffer[offset .. offset + 5]);
 }
